d499798b71cce6ffcac08b42cc376d20bcae3db9
Yevgeniy Kiveisha <yevgeniy.kiveisha@gmail.com>
2020-09-03 16:12:39 +0300

    TX/RX Packets

--- gateway.js
+++ gateway.js
@@ -852,7 +852,6 @@ MkSGateway.prototype.Start = function () {
 													break;
 													case "node_uninstall": {
 														console.log(self.ModuleName, (new Date()), "Uninstall node ", payload.node.uuid);
-														self.Database.RemoveNode(payload.node);
 													}
 													break;
 													case "node_connected": {
